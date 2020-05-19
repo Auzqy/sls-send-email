035c3b5ddf7dc9e0208e5a2ac384e7aa7a3dfafd
Auzqy <247923631@qq.com>
2020-05-19 19:27:24 +0800

    Update index.php
    add some note for how to use by myself

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   use RingCentral\Psr7\Response;
   use Dm\Request\V20151123 as Dm;
   //需要设置对应的region名称，如华东1（杭州）设为cn-hangzhou，新加坡Region设为ap-southeast-1，澳洲Region设为ap-southeast-2。
+  // au note: 经过测试 region 设置为 cn-hangzhou 或者 cn-shanghai 均可
   $iClientProfile = DefaultProfile::getProfile("cn-shanghai", "your_accessKeyID", "your_accessKeySecret");
   //新加坡或澳洲region需要设置服务器地址，华东1（杭州）不需要设置。
   //$iClientProfile::addEndpoint("ap-southeast-1","ap-southeast-1","Dm","dm.ap-southeast-1.aliyuncs.com");
@@ -11,11 +12,13 @@
   $request = new Dm\SingleSendMailRequest();
   //新加坡或澳洲region需要设置SDK的版本，华东1（杭州）不需要设置。
   //$request->setVersion("2017-06-22");
+  // au note: 此处要替换为 邮件服务内配置的 “触发邮件的发信地址”
   $request->setAccountName("message@mail.jike-serverless.online");
   $request->setFromAlias("秦粤");
   $request->setAddressType(1);
   $request->setTagName("capture");
   $request->setReplyToAddress("false");
+// au note: 此处替换为 目标收信人的收信地址
   $request->setToAddress("ukyo.pu@gmail.com");
   //可以给多个收件人发送邮件，收件人之间用逗号分开,若调用模板批量发信建议使用BatchSendMailRequest方式
   //$request->setToAddress("邮箱1,邮箱2");
